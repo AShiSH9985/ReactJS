521ae7ec8f01b0fec736d4b464edca0833bde8ba
Ashish <ashish8979778599@gmail.com>
2026-06-06 17:24:10 +0530

    props

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <>
+     <div className='parent'>
+        <Card name="Ashish" src="https://images.unsplash.com/photo-1499209974431-9dddcece7f88?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGZyZWUlMjBpbWFnZXN8ZW58MHx8MHx8fDA%3D"/>
+
+     </div>
+    </>
+  )
+}
+
+export default App
