211b199e9817202ccac60228739ed6633a35e000
Ashish <ashish8979778599@gmail.com>
2026-06-07 16:03:13 +0530

    two-way-binding in form handling

---
+++ two-way-binding/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [title, settitle] = useState('')
+
+  const submitHandler=(e)=>{
+    e.preventDefault()
+    console.log(`form subtmitted by ${title}`)
+    settitle('')
+  }
+  return (
+    <div>
+      <form  onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input type="text" placeholder='Enter your name'
+        value={title} onChange={(e)=>{
+          settitle(e.target.value)
+        }}/>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
