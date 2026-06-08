6db8e7001a02392cf8949486f3d4e98c08f02f4d
Ashish <ashish8979778599@gmail.com>
2026-06-08 16:22:55 +0530

    Api

---
+++ Backend/src/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app =express()
+app.use(express.json())
+
+const notes=[]
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})  
+app.get("/about",(req,res)=>{
+    res.send("About world")
+})
+
+app.post('/notes',(req,res)=>{
+    notes.push(req.body)
+    res.status(201).json({
+        message:"note created successfully"
+    })
+})
+
+app.get("/notes",(req,res)=>{
+    res.status(200).json({
+        message:"notes fetched successfully",
+        notes:notes
+    })
+})
+
+app.delete("/notes/:index",(req,res)=>{
+    const index=req.params.index
+    delete notes[index]
+    res.status(200).json({
+        message:"note deleted successfully",
+        notes:notes
+    })
+})
+app.patch("/notes/:index",(req,res)=>{
+    const index=req.params.index
+    const {description}=req.body
+    notes[index].description=description
+    res.status(200).json({
+        message:"note updated successfully"
+    })
+})
+
+module.exports=app
